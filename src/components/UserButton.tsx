895ca0451f2b0f198cbde51563595c739bcc9d11
igor <cruigor@gmail.com>
2024-08-20 16:15:09 -0300

    create navbar

--- src/components/UserButton.tsx
+++ src/components/UserButton.tsx
@@ -1,8 +1,19 @@
-"use client"
+"use client";
 
 import { useSession } from "@/app/(main)/SessionProvider";
-import { DropdownMenu } from "./ui/dropdown-menu";
-import { DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import UserAvatar from "./UserAvatar";
+import Link from "next/link";
+import { LogOutIcon, UserIcon } from "lucide-react";
+import { logout } from "@/app/(auth)/actions";
+import { cn } from "@/lib/utils";
 
 interface UserButtonProps {
   className?: string;
@@ -13,8 +24,29 @@ export default function UserButton({ className }: UserButtonProps) {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-
+        <button className={cn("flex-none rounded-full", className)}>
+          <UserAvatar avatarUrl={user.avatarUrl} size={40} />
+        </button>
       </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>Logged in as @{user.username}</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <Link href={`/users/${user.username}`}>
+          <DropdownMenuItem>
+            <UserIcon className="mr-2 size-4" />
+            Profile
+          </DropdownMenuItem>
+        </Link>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem
+          onClick={() => {
+            logout();
+          }}
+        >
+          <LogOutIcon className="mr-2 size-4" />
+          Logout
+        </DropdownMenuItem>
+      </DropdownMenuContent>
     </DropdownMenu>
   );
 }
