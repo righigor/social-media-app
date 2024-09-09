e8ae6addc3aab3969ffcb8904c6b9544712debb6
igor <cruigor@gmail.com>
2024-09-09 18:13:36 -0300

    correcao

--- src/app/(main)/search/page.tsx
+++ src/app/(main)/search/page.tsx
@@ -1,27 +1,29 @@
-import TrendsSideBar from "@/components/TrendsSideBar"
-import { Metadata } from "next"
-import SearchResults from "./SearchResult"
+import { Metadata } from "next";
+import SearchResults from "./SearchResult";
+import TrendsSideBar from "@/components/TrendsSideBar";
 
 interface PageProps {
-  searchParams: { q: string}
+  searchParams: { q: string };
 }
 
-export function generateMatadata({ searchParams: { q } }: PageProps): Metadata {
+export function generateMetadata({ searchParams: { q } }: PageProps): Metadata {
   return {
     title: `Search results for "${q}"`,
-  }
+  };
 }
 
-export default function SearchPage({ searchParams: {q} }: PageProps) {
+export default function Page({ searchParams: { q } }: PageProps) {
   return (
     <main className="flex w-full min-w-0 gap-5">
       <div className="w-full min-w-0 space-y-5">
         <div className="rounded-2xl bg-card p-5 shadow-sm">
-          <h1 className="text-center text-2xl font-bold line-clamp-2 break-all">Search results for &quot;{q}&quot;</h1>
+          <h1 className="line-clamp-2 break-all text-center text-2xl font-bold">
+            Search results for &quot;{q}&quot;
+          </h1>
         </div>
         <SearchResults query={q} />
       </div>
       <TrendsSideBar />
     </main>
-  )
+  );
 }
